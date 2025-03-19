cd9f51490b50ac9272cf7517fdf676f363c52772
Arnold Agaba <arnoldagba2@gmail.com>
2025-03-19 08:03:44 +0300

    Initialise the backend

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,3 +1,20 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+const app: Express = express();
+const PORT = process.env.PORT ?? 5000;
+
+//
+app.use(helmet());
+app.use(cors());
+
+// API Endpoints
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "IMS API is running",
+    });
+});
+
+// Start the Server
+app.listen(PORT, () => console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`));
